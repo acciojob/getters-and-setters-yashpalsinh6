4c29ed8ca198bd33689085602e7e17ea3f61fb14
yashpalsinh6 <75825134+yashpalsinh6@users.noreply.github.com>
2024-04-30 11:26:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,74 @@
 //complete this code
-class Person {}
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Student extends Person {}
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  set age(age) {
+    this._age = age;
+  }
 
+  get age() {
+    return this._age;
+  }
+}
+
+class Student extends Person {
+  study() {
+    console.log(`${this._name} is studying`);
+  }
+}
+
+class Teacher extends Person {
+  teach() {
+    console.log(`${this._name} is teaching`);
+  }
+}
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
+
+
+
+
+
+// //complete this code
+// class Person {
+// 	constructor(name, age){
+// 		this._name = name;
+// 		this._age = age;
+// 	}
+// 	get name(){
+// 		return this._name;
+// 	}
+// 	set age(){
+// 		this._age = age;
+// 	}
+// 	get age(){
+// 		return this._age;
+// 	}
+	
+// }
+
+// class Student extends Person {
+// 	study(){
+// 		console.log(`${this._name} is studying`);
+// 	}
+// }
+
+// class Teacher extends Person {
+// 	teach(){
+// 		console.log(`${this._name} is teaching`);
+// 	}
+// }
+
+// // Do not change the code below this line
+// window.Person = Person;
+// window.Student = Student;
+// window.Teacher = Teacher;
